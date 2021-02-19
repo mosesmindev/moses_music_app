63e967ba717d949b01513b137e27b28aa16165f1
mosesmindev <PeterChenjinxu@outlook.com>
2021-02-19 08:23:02 +0800

    003次提交：首页activity创建和首页布局框架搭建。

---
+++ moses_voice/src/main/java/com/moses/moses_voice/view/home/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.moses.moses_voice.view.home;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.moses.moses_voice.R;
+
+/**
+ * 首页
+ */
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        initData();
+        
+    }
+
+    private void initView() {
+    }
+
+    private void initData() {
+    }
+}
